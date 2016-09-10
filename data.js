941c47901fc54e2cbf75e029a6badf723ccfd832
Enrico Cambiaso <enrico.cambiaso@gmail.com>
2016-09-10 10:18:14 +0200

    Added support to disabled channels

--- data.js
+++ data.js
@@ -7,62 +7,80 @@ var data = {
 			'rai1':{
 				'name':'Rai 1',
 				'img':'img/rai_rai1.png',
-				'video':'http://raiuno1hls-lh.akamaihd.net/i/raiuno1_1@37570/index_350_av-p.m3u8?sd=10&rebase=on'
+				'video':'http://raiuno1hls-lh.akamaihd.net/i/raiuno1_1@37570/index_350_av-p.m3u8?sd=10&rebase=on',
+				'enabled':'false'
 			},
 			'rai1hd':{
 				'name':'Rai 1 HD',
 				'img':'img/rai_rai1hd.png',
-				'video':'http://raiuno1hls-lh.akamaihd.net/i/raiuno1_1@37570/index_1200_av-b.m3u8?sd=10&rebase=on'
+				'video':'http://raiuno1hls-lh.akamaihd.net/i/raiuno1_1@37570/index_1200_av-b.m3u8?sd=10&rebase=on',
+				'enabled':'false'
 			},
 			'rai2':{
 				'name':'Rai 2',
 				'img':'img/rai_rai2.png',
-				'video':'http://raidue1hls-lh.akamaihd.net/i/raidue1_1@322403/index_350_av-p.m3u8?sd=10&rebase=on'
+				'video':'http://raidue1hls-lh.akamaihd.net/i/raidue1_1@322403/index_350_av-p.m3u8?sd=10&rebase=on',
+				'enabled':'false'
 			},
 			'rai2hd':{
 				'name':'Rai 2 HD',
 				'img':'img/rai_rai2hd.png',
-				'video':'http://raidue1hls-lh.akamaihd.net/i/raidue1_1@322403/index_1200_av-b.m3u8?sd=10&rebase=on'
+				'video':'http://raidue1hls-lh.akamaihd.net/i/raidue1_1@322403/index_1200_av-b.m3u8?sd=10&rebase=on',
+				'enabled':'false'
 			},
 			'rai3':{
 				'name':'Rai 3',
 				'img':'img/rai_rai3.png',
-				'video':'http://raitre1hls-lh.akamaihd.net/i/raitre1_1@35171/index_350_av-p.m3u8?sd=10&rebase=on'
+				'video':'http://raitre1hls-lh.akamaihd.net/i/raitre1_1@35171/index_350_av-p.m3u8?sd=10&rebase=on',
+				'enabled':'false'
 			},
 			'rai3hd':{
 				'name':'Rai 3 HD',
 				'img':'img/rai_rai3hd.png',
-				'video':'http://raitre1hls-lh.akamaihd.net/i/raitre1_1@35171/index_1200_av-b.m3u8?sd=10&rebase=on'
+				'video':'http://raitre1hls-lh.akamaihd.net/i/raitre1_1@35171/index_1200_av-b.m3u8?sd=10&rebase=on',
+				'enabled':'false'
 			},
 			'rai4':{
 				'name':'Rai 4',
 				'img':'img/rai_rai4.png',
-				'video':'http://raiquattro1hls-lh.akamaihd.net/i/raiquattro1_1@335025/index_350_av-p.m3u8?sd=10&rebase=on'
+				'video':'http://raiquattro1hls-lh.akamaihd.net/i/raiquattro1_1@335025/index_350_av-p.m3u8?sd=10&rebase=on',
+				'enabled':'true'
 			},
 			'rai4hd':{
 				'name':'Rai 4 HD',
 				'img':'img/rai_rai4hd.png',
-				'video':'http://raiquattro1hls-lh.akamaihd.net/i/raiquattro1_1@335025/index_1200_av-b.m3u8?sd=10&rebase=on'
+				'video':'http://raiquattro1hls-lh.akamaihd.net/i/raiquattro1_1@335025/index_1200_av-b.m3u8?sd=10&rebase=on',
+				'enabled':'true'
 			},
 			'rai5':{
 				'name':'Rai 5',
 				'img':'img/rai_rai5.png',
-				'video':'http://b2everyrai-lh.akamaihd.net/i/rai5_1@182695/index_350_av-p.m3u8?sd=10&rebase=on'
+				'video':'http://b2everyrai-lh.akamaihd.net/i/rai5_1@182695/index_350_av-p.m3u8?sd=10&rebase=on',
+				'enabled':'true'
 			},
 			'rai5hd':{
 				'name':'Rai 5 HD',
 				'img':'img/rai_rai5hd.png',
-				'video':'http://b2everyrai-lh.akamaihd.net/i/rai5_1@182695/index_1200_av-b.m3u8?sd=10&rebase=on'
+				'video':'http://b2everyrai-lh.akamaihd.net/i/rai5_1@182695/index_1200_av-b.m3u8?sd=10&rebase=on',
+				'enabled':'true'
+			},
+			'raipremium':{
+				'name':'Rai Premium',
+				'img':'img/rai_raipremium.png',
+				'video':'http://b2everyrai-lh.akamaihd.net/i/raipremium_1@181837/index_1200_av-p.m3u8',
+				'enabled':'true'
 			},
 			'rainews':{
 				'name':'Rai News',
 				'img':'img/rai_rainews.png',
-				'video':'http://b2everyrai-lh.akamaihd.net/i/rainews_1@179616/index_350_av-p.m3u8?sd=10&rebase=on'
+				'video':'http://b2everyrai-lh.akamaihd.net/i/rainews_1@179616/index_350_av-p.m3u8?sd=10&rebase=on',
+				'enabled':'true'
 			},
 			'rainewshd':{
 				'name':'Rai News HD',
 				'img':'img/rai_rainewshd.png',
-				'video':'http://b2everyrai-lh.akamaihd.net/i/rainews_1@179616/index_1200_av-b.m3u8?sd=10&rebase=on'
+				'video':'http://b2everyrai-lh.akamaihd.net/i/rainews_1@179616/index_1200_av-b.m3u8?sd=10&rebase=on',
+				'enabled':'true'
 			}
 		}
 	},
@@ -74,47 +92,56 @@ var data = {
 			'canale5':{
 				'name':'Canale Cinque',
 				'img':'img/mediaset_canale5.png',
-				'video':'http://se-ge1-1.se.live2.msf.ticdn.it/Content/HLS/Live/Channel(CH01HA)/index.m3u8'
+				'video':'http://se-ge1-1.se.live2.msf.ticdn.it/Content/HLS/Live/Channel(CH01HA)/index.m3u8',
+				'enabled':'true'
 			},
 			'italia1':{
 				'name':'Italia Uno',
 				'img':'img/mediaset_italia1.png',
-				'video':'http://live2.msf.ticdn.it/Content/HLS/Live/Channel(CH02HA)/index.m3u8'
+				'video':'http://live2.msf.ticdn.it/Content/HLS/Live/Channel(CH02HA)/index.m3u8',
+				'enabled':'true'
 			},
 			'rete4':{
 				'name':'Retequattro',
 				'img':'img/mediaset_rete4.png',
-				'video':'http://live2.msf.ticdn.it/Content/HLS/Live/Channel(CH03HA)/index.m3u8'
+				'video':'http://live2.msf.ticdn.it/Content/HLS/Live/Channel(CH03HA)/index.m3u8',
+				'enabled':'true'
 			},
 			'la5':{
 				'name':'La 5',
 				'img':'img/mediaset_la5.png',
-				'video':'http://live2.msf.ticdn.it/Content/HLS/Live/Channel(CH04HA)/index.m3u8'
+				'video':'http://live2.msf.ticdn.it/Content/HLS/Live/Channel(CH04HA)/index.m3u8',
+				'enabled':'true'
 			},
 			'italia2':{
 				'name':'Italia 2',
 				'img':'img/mediaset_italia2.png',
-				'video':'http://live2.msf.ticdn.it/Content/HLS/Live/Channel(CH05HA)/index.m3u8'
+				'video':'http://live2.msf.ticdn.it/Content/HLS/Live/Channel(CH05HA)/index.m3u8',
+				'enabled':'true'
 			},
 			'mediasetextra':{
 				'name':'Mediaset Extra',
 				'img':'img/mediaset_mediasetextra.png',
-				'video':'http://live2.msf.ticdn.it/Content/HLS/Live/Channel(CH09HA)/index.m3u8'
+				'video':'http://live2.msf.ticdn.it/Content/HLS/Live/Channel(CH09HA)/index.m3u8',
+				'enabled':'true'
 			},
 			'topcrime':{
 				'name':'Top Crime',
 				'img':'img/mediaset_topcrime.png',
-				'video':'http://se-ge1-1.se.live2.msf.ticdn.it/Content/HLS/Live/Channel(CH07HA)/index.m3u8'
+				'video':'http://se-ge1-1.se.live2.msf.ticdn.it/Content/HLS/Live/Channel(CH07HA)/index.m3u8',
+				'enabled':'true'
 			},
 			'iris':{
 				'name':'Iris',
 				'img':'img/mediaset_iris.png',
-				'video':'http://live2.msf.ticdn.it/Content/HLS/Live/Channel(CH06HA)/index.m3u8'
+				'video':'http://live2.msf.ticdn.it/Content/HLS/Live/Channel(CH06HA)/index.m3u8',
+				'enabled':'true'
 			},
 			'tgcom24':{
 				'name':'Tgcom24',
 				'img':'img/mediaset_tgcom24.png',
-				'video':'http://live2.msf.ticdn.it/Content/HLS/Live/Channel(CH10HA)/index.m3u8'
+				'video':'http://live2.msf.ticdn.it/Content/HLS/Live/Channel(CH10HA)/index.m3u8',
+				'enabled':'true'
 			}
 		}
 	},
@@ -126,12 +153,14 @@ var data = {
 			'cielo':{
 				'name':'Cielo',
 				'img':'img/sky_cielo.png',
-				'video':'http://skyianywhere2-i.akamaihd.net/hls/live/216865/cielo/playlist.m3u8'
+				'video':'http://skyianywhere2-i.akamaihd.net/hls/live/216865/cielo/playlist.m3u8',
+				'enabled':'true'
 			},
 			'skytg24':{
 				'name':'Sky TG24',
 				'img':'img/sky_skytg24.png',
-				'video':'http://skyianywhere2-i.akamaihd.net/hls/live/200275/tg24/playlist.m3u8'
+				'video':'http://skyianywhere2-i.akamaihd.net/hls/live/200275/tg24/playlist.m3u8',
+				'enabled':'true'
 			}
 		}
 	}
